34239543ffbe01bfb8b207f562b84f6777c6d36a
Eric Richter <richteer@clarkson.edu>
2015-03-25 11:26:47 -0400

    implemented inode_next and inode_alloc

--- src/inode.c
+++ src/inode.c
@@ -15,10 +15,10 @@ int inode_get_location(int inode_number)
 	whichInodeBlock = inode_number/INODES_PER_BLOCK;
 	whichInodeInBlock = inode_number%INODES_PER_BLOCK;
 
-	return (INODE_BLOCK + whichInodeBlock) *BLOCK_SIZE_BYTES + whichInodeInBlock*sizeof(inode);
+	return (INODE_BLOCK + whichInodeBlock) *BLOCK_SIZE_BYTES + whichInodeInBlock*sizeof(inode_t);
 }
 
-int inode_write(int inode_number, inode * in)
+int inode_write(int inode_number, inode_t * in)
 {
 	int inodeLocation;
 	assert(inode_number < MAX_INODES);
@@ -26,7 +26,7 @@ int inode_write(int inode_number, inode * in)
 	inodeLocation = inode_get_location(inode_number);
   	in->last_modified = time(NULL);
 	lseek(virtual_disk, inodeLocation, SEEK_SET);
-	crash_write(virtual_disk, in, sizeof(inode));
+	crash_write(virtual_disk, in, sizeof(inode_t));
   
 	sync();
 
@@ -34,7 +34,7 @@ int inode_write(int inode_number, inode * in)
 }
 
 
-int inode_read(int inode_number, inode * in) 
+int inode_read(int inode_number, inode_t * in) 
 {
 	int inodeLocation;
 	assert(inode_number < MAX_INODES);
@@ -43,21 +43,29 @@ int inode_read(int inode_number, inode * in)
 
   
 	lseek(virtual_disk, inodeLocation, SEEK_SET);
-	read(virtual_disk, in, sizeof(inode));
+	read(virtual_disk, in, sizeof(inode_t));
   
 	return 1;
 }
 
-/* TODO
-   Sets an inode as allocated
-*/
-void inode_alloc(inode * in, int blocks, int size) {
+void inode_alloc(inode_t * in, int blocks, int size)
+{
+	in->num_blocks = blocks;
+	in->file_size_bytes = size;
+	in->free = 0;
+	in->last_modified = time(NULL);
 }
 
-/* TODO
-   Returns the next free inode.
-*/
-int inode_next() {
+int inode_next()
+{
+	int i;
+	inode_t in;
+
+	for (i = 0; i < MAX_INODES; i++) {
+		inode_read(i, &in);
+		if (in.free) return i;
+	}
+
 	return -1;
 }
 
